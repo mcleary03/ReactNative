3d640fff59ca015dd6ecb70f3e7705320b1c3c8e
Michael Cleary <mcleary03@gmail.com>
2017-04-04 16:16:35 -0700

    add Card styles and applied them to AlbumList

--- src/components/AlbumDetail.js
+++ src/components/AlbumDetail.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Text } from 'react-native';
+// wrapping this with Card styles
+import Card from './Card';
 
 // props comes from the rederAlbums function in AlbumList.js
 const AlbumDetail = (props) => {
+  // since we are passing Card this Text component,
+  // we need to go tell Card how to render it.
   return (
-    <View>
+    <Card>
       <Text>{props.album.title}</Text>
-    </View>
+    </Card>
   );
 };
 
